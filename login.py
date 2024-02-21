dadd0541b304747f4bf084ac625bb4ca0d47f5bf
Rowby Pierre <rowbypierre1998@gmail.com>
2024-02-21 02:12:06 -0500

    update prompts, add cleared screens after each username and password attempt

--- login.py
+++ login.py
@@ -1,4 +1,4 @@
-import sys, time 
+import sys, os, time 
 from getpass import getpass
 
 def logon():
@@ -12,43 +12,61 @@ def logon():
         
         print("")
         username = input("Enter username: ").lower().strip()
-        time.sleep(1)
+        time.sleep(.75)
         uthreshold += 1
     
-        if uthreshold == 9:
-            print("")
-            print("Last attempt before script terminates.")
-            time.sleep(1)
-        elif (uthreshold == 10) and (username != xusername):
-            print("")
-            print("Terminating script...")
-            sys.exit()
-        elif username == xusername:
+        if username == xusername:
             username = username.capitalize()
             print("")
             print(f"Welcome {username}")
-            time.sleep(1)
-        
+            time.sleep(.75)
+            os.system("clear")
+            
             while status == "locked":
                 
                 print("")
                 password = getpass("Enter password: ").lower().strip()
-                time.sleep(1)
+                time.sleep(.75)
                 pthreshold += 1
             
-                if (pthreshold == 9):
-                    print("")
-                    print("Last attempt before script terminates.")
-                    time.sleep(1)
-                if pthreshold >= 1:
-                    print("")
-                    print("Incorrect password entrered for: " + username)
-                elif pthreshold == 10 and (password != xpassword):
-                    print("")
-                    print("Terminating script...")
-                    sys.exit()
                 if password == xpassword:
                     print("")
                     print(f"Correct password entered for username: {username}")
-                    time.sleep(1)
+                    time.sleep(.75)
                     status = "unlocked"
+                    os.system("clear")
+                else:
+                    if pthreshold == 9:
+                        print("")
+                        print("Last attempt before program terminates.")
+                        time.sleep(.75)
+                        os.system("clear")
+                    if pthreshold in range (1, 8):
+                        print("")
+                        print("Incorrect password entered for: " + username)
+                        time.sleep(.75)
+                        os.system("clear")
+                    elif pthreshold == 10:
+                        print("")
+                        print("Terminating program...")
+                        os.system("clear")
+                        sys.exit()
+                        
+        else:
+            print("")
+            print("Incorrect username entered.")
+            time.sleep(.75)
+            os.system("clear")
+            if uthreshold == 9:
+                print("")
+                print("Last attempt before program terminates.")
+                time.sleep(.75)
+            elif (uthreshold == 10):
+                print("")
+                print("Terminating program...")
+                os.system("clear")
+                sys.exit()
+                
+    
+        
+            
